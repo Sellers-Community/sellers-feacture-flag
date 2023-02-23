7639612b62233cf889ece2b7c54ab57ed60dda44
ArielTintel <arielandrade97@gmail.com>
2023-02-23 11:30:38 -0300

    Adicionado anotação FeignClients

--- feacture-flag-clients/src/main/java/com/br/sellers/feactureflagclient/FeactureFlagClientApplication.java
+++ feacture-flag-clients/src/main/java/com/br/sellers/feactureflagclient/FeactureFlagClientApplication.java
@@ -2,8 +2,10 @@ package com.br.sellers.feactureflagclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FeactureFlagClientApplication {
 
 	public static void main(String[] args) {
